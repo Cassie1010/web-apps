8eb1dad2766a7f0c5e3fd8526ae0ad15f3a897df
zuomengmeng <zuomm@tellhow.com>
2021-12-23 17:12:51 +0800

    打开内容控制

--- apps/documenteditor/main/app/controller/Main.js
+++ apps/documenteditor/main/app/controller/Main.js
@@ -394,7 +394,7 @@ define([
                 this.appOptions.canRequestSharingSettings = this.editorConfig.canRequestSharingSettings;
                 this.appOptions.compatibleFeatures = (typeof (this.appOptions.customization) == 'object') && !!this.appOptions.customization.compatibleFeatures;
                 this.appOptions.canFeatureComparison = !!this.api.asc_isSupportFeature("comparison");
-                this.appOptions.canFeatureContentControl = !!this.api.asc_isSupportFeature("content-controls");
+                this.appOptions.canFeatureContentControl = true;
                 this.appOptions.canFeatureForms = false;
                 this.appOptions.mentionShare = !((typeof (this.appOptions.customization) == 'object') && (this.appOptions.customization.mentionShare==false));
 
